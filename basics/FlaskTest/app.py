57774ef4314c7d0cf8735b9d938c4086ff4c3706
loriyuhv <loriyuhv@qq.com>
2021-10-19 13:34:35 +0800

    commit 增加两个可视化 @loriyuhv

--- basics/FlaskTest/app.py
+++ basics/FlaskTest/app.py
@@ -11,12 +11,50 @@ app = Flask(__name__)
 @app.route('/1')
 def bar():
     data = GetData()
+    # print(data)
     xData = []
     yData = []
     for i in data:
-        xData.append(i[1])
-        yData.append(i[0])
+        xData.append(i[0])
+        yData.append(i[1])
     return render_template("bar.html", data={"xData": xData, "yData": yData})
+    pass
+
+
+# 借款等级分布图——堆叠条形图
+@app.route("/2")
+def double_bar():
+    data = GetData()
+    # print(data)
+    fData = []
+    vData = []
+    grade = [1, 2, 3, 4, 5]
+    for j in range(0, 5):
+        for i in range(j * 7, j * 7 + 7):
+            vData.append(data[i][2])
+        # print(fData[j])
+        fData.append({"category": grade[j], "value": vData})
+        vData = []
+
+    return render_template("double_bar.html", data=fData)
+    pass
+
+
+# 借款等级与借款金额分布图——堆叠条形图
+@app.route("/3")
+def grade_amount():
+    data = GetData()
+    fData = []
+    vData = []
+    grade = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    for j in range(0, len(grade)):
+        for i in range(j * 5, j * 5 + 5):
+            vData.append(data[i][2])
+        # print(fData[j])
+        fData.append({"category": grade[j], "value": vData})
+        vData = []
+
+    return render_template("grade_amount.html", data=fData)
     pass
 
 
@@ -31,10 +69,14 @@ def GetData():
     )
     cursor = connect.cursor()
     # 1借款金额分布
-    cursor.execute("SELECT * FROM loan_amount;")
+    # cursor.execute("SELECT * FROM loan_amount ORDER BY CAST(SUBSTRING_INDEX(zone,'-',1) AS SIGNED);")
+    # 2借款等级分布 需要注释掉上一条命令
+    # cursor.execute("SELECT * FROM loan_grade ORDER BY RIGHT(subgrade,1),grade")
+    # 3借款等级与借款金额的关系 需要注释掉上一条命令
+    cursor.execute("SELECT * FROM grade_amount ORDER BY grade,CAST(SUBSTRING_INDEX(amount,'-',1) AS SIGNED)")
     # 从mysql提取数据到data
     data = cursor.fetchall()
-
+    print(data)
     cursor.close()
     connect.close()
     return [i for i in data]
